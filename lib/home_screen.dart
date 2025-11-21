31133c48c739dbab97b8f733a07fc773a11da3f6
nicoxkd <150947751+nicoxkd@users.noreply.github.com>
2025-11-21 11:04:50 +0100

    Perfiles funcionando

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,116 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:viniloteca/login_screen.dart'; // Para navegar a Login
-import 'tiendas.dart'; // Importa la página de tiendas
-import 'foros.dart';
+import 'package:viniloteca/login_screen.dart'; 
+import 'package:viniloteca/profile_screen.dart'; // Importa la pantalla de perfil
 
-// --- WIDGET DE LA CABECERA (ACTUALIZADO) ---
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  
   final String? nombreUsuario;
+  final int? userId; // ID del usuario logueado
 
-  const MyAppBar({
-    super.key, 
-    this.nombreUsuario 
-  });
+  const MyAppBar({super.key, this.nombreUsuario, this.userId});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
         color: Color(0xFFe6d5b5),
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.black,
-            width: 3.0,
-          ),
-        ),
+        border: Border(bottom: BorderSide(color: Colors.black, width: 3.0)),
       ),
       child: SafeArea(
         child: Row(
           children: [
-            // 1. Icono de Menú (Izquierda)
+            // Menú Hamburguesa
             Expanded(
               flex: 1,
               child: IconButton(
                 icon: const Icon(Icons.menu, size: 35, color: Colors.black),
-                onPressed: () { 
-                  if (Navigator.canPop(context)) {
-                    Navigator.pop(context);
-                  } else {
-                    print('Menú presionado en Home'); 
-                  }
-                },
+                onPressed: () {},
               ),
             ),
-            
-            // 2. LOGO CENTRAL (logo.png)
+            // Logo
             Expanded(
               flex: 2,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomeScreen(nombreUsuario: nombreUsuario),
-                    ),
-                    (route) => false,
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0), 
-                  child: Image.asset(
-                    'assets/logo.png',
-                    fit: BoxFit.contain,
-                    height: 75,
-                  ),
-                ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0), 
+                child: Image.asset('assets/logo.png', fit: BoxFit.contain, height: 75),
               ),
             ),
-
-            // --- 3. ICONO DE PERFIL (DINÁMICO) ---
+            // Perfil (Invitado o Logueado)
             (nombreUsuario == null)
               ? _buildLoginIcon(context)
-              : _buildProfileMenu(context, nombreUsuario!),
+              : _buildProfileMenu(context, nombreUsuario!, userId!),
           ],
         ),
       ),
     );
   }
 
-  // WIDGET PARA INVITADO
   Widget _buildLoginIcon(BuildContext context) {
     return Expanded(
       flex: 1,
       child: InkWell(
         onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const LoginScreen()),
-          );
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
         },
         child: Container(
-          height: double.infinity, 
-          color: Colors.red,
+          height: double.infinity, color: Colors.red,
           child: const Icon(Icons.person, size: 35, color: Colors.white),
         ),
       ),
     );
   }
 
-  // WIDGET PARA USUARIO LOGUEADO
-  Widget _buildProfileMenu(BuildContext context, String nombre) {
+  Widget _buildProfileMenu(BuildContext context, String nombre, int id) {
     return Expanded(
       flex: 1,
       child: Container(
-        height: double.infinity,
-        color: Colors.red,
+        height: double.infinity, color: Colors.red,
         child: PopupMenuButton<String>(
           onSelected: (value) {
             if (value == 'logout') {
               Navigator.pushAndRemoveUntil(
                 context,
-                MaterialPageRoute(builder: (context) => const HomeScreen(nombreUsuario: null)), 
+                MaterialPageRoute(builder: (context) => const HomeScreen()), 
                 (route) => false,
+              );
+            } else if (value == 'profile') {
+              // NAVEGAR AL PERFIL
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfileScreen(userId: id)),
               );
             }
           },
@@ -118,16 +84,11 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
           itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
             PopupMenuItem<String>(
               enabled: false, 
-              child: Text(
-                nombre,
-                style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-              ),
+              child: Text(nombre, style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
             ),
             const PopupMenuDivider(),
-            const PopupMenuItem<String>(
-              value: 'logout',
-              child: Text('Cerrar Sesión'),
-            ),
+            const PopupMenuItem<String>(value: 'profile', child: Text('Ir al perfil')),
+            const PopupMenuItem<String>(value: 'logout', child: Text('Cerrar Sesión')),
           ],
         ),
       ),
@@ -138,63 +99,31 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => const Size.fromHeight(100.0); 
 }
 
-
-// --- PANTALLA PRINCIPAL (ACTUALIZADA) ---
 class HomeScreen extends StatelessWidget {
-  
   final String? nombreUsuario;
+  final int? userId;
 
-  const HomeScreen({
-    super.key, 
-    this.nombreUsuario
-  });
+  const HomeScreen({super.key, this.nombreUsuario, this.userId});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(nombreUsuario: nombreUsuario), 
+      appBar: MyAppBar(nombreUsuario: nombreUsuario, userId: userId), 
       backgroundColor: const Color(0xFF800000), 
       body: SingleChildScrollView(
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 const SizedBox(height: 30),
-
-                // Tarjeta 1: TIENDAS
                 _buildMenuCard(
-                  context: context, 
-                  label: 'TIENDAS',
-                  imagePath: 'assets/imagen.png',
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => StoreListPage(nombreUsuario: nombreUsuario),
-                      ),
-                    );
-                  },
+                  context: context, label: 'TIENDAS', imagePath: 'assets/imagen.png', hasBorder: false, onTap: () {},
                 ),
-
                 const SizedBox(height: 30),
-
-              _buildMenuCard(
-                context: context, 
-                label: 'FOROS',
-                imagePath: 'assets/imagenFondo1.png',                
-                // --- AQUÍ ESTÁ EL CAMBIO ---
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      // Pasamos el nombreUsuario para mantener la sesión en la cabecera
-                      builder: (context) => ForumListPage(nombreUsuario: nombreUsuario),
-                    ),
-                  );
-                },
-              ),
+                _buildMenuCard(
+                  context: context, label: 'FOROS', imagePath: 'assets/imagenFondo1.png', hasBorder: false, onTap: () {},
+                ),
               ],
             ),
           ),
@@ -203,55 +132,27 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  // --- WIDGET REUTILIZABLE PARA LAS TARJETAS ---
-  Widget _buildMenuCard({
-    required BuildContext context, 
-    required String label,
-    required String imagePath,
-    required VoidCallback onTap,
-  }) {
+  Widget _buildMenuCard({required BuildContext context, required String label, required String imagePath, required VoidCallback onTap, bool hasBorder = false}) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final squareSize = screenWidth * 0.55; 
+    final squareSize = screenWidth * 0.55;
 
     return InkWell(
       onTap: onTap,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image.asset(
-            imagePath,
-            width: squareSize,
-            height: squareSize,
-            fit: BoxFit.cover,
-            errorBuilder: (context, error, stackTrace) {
-              return Container(
-                width: squareSize,
-                height: squareSize,
-                color: Colors.grey[300],
-                child: Center(
-                  child: Text(
-                    'Error al cargar: $imagePath', 
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.red, fontSize: 16),
-                  ),
-                ),
-              );
-            },
-          ),
-          Container(
-            color: Colors.white,
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-            child: Text(
-              label,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.5,
-              ),
+      child: Container(
+        padding: hasBorder ? const EdgeInsets.all(4.0) : EdgeInsets.zero,
+        decoration: hasBorder ? BoxDecoration(border: Border.all(color: Colors.purple, width: 3)) : null,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(imagePath, width: squareSize, height: squareSize, fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) => Container(width: squareSize, height: squareSize, color: Colors.grey[300])),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              child: Text(label, style: const TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 1.5)),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
